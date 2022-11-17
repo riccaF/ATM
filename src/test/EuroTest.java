f97d0f61b75b5ba43517f5b615da5ee7e90fb87f
federico conti <federicoconti1702@gmail.com>
2022-11-17 12:15:21 +0100

    Merge Feature-->Test,Inizializzato JUnit Aggiunto Import Euro

---
+++ src/test/EuroTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import org.junit.Test;
+
+import code.Business_logic.Euro; 
+
+public class EuroTest {
+
+    @Test
+    public void testGetValore() {
+       
+    }
+
+    @Test
+    public void testMinoreDi() {
+
+    }
+
+    @Test
+    public void testSomma() {
+
+    }
+
+    @Test
+    public void testSottrai() {
+
+    }
+
+    @Test
+    public void testStampa() {
+
+    }
+
+    @Test
+    public void testUgualeA() {
+
+    }
+}
