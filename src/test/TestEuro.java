30f15492991b7436da1671d080e4442cae52803a
iobah <s5077128@studenti.unige.it>
2022-11-17 14:17:45 +0100

    added TestMinoreDi e testStampa

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -1,6 +1,10 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import code.Business_logic.Euro;
 
 public class TestEuro {
     // TODO(arkeane)
@@ -12,7 +16,9 @@ public class TestEuro {
     // TODO(iobah)
     @Test
     public void testMinoreDi() {
-
+        Euro a= new Euro(1);
+        Euro b=new Euro(2);
+        assertEquals(true , a.minoreDi(b));
     }
 
     // TODO(anubi0)
@@ -30,7 +36,8 @@ public class TestEuro {
     // TODO(iobah)
     @Test
     public void testStampa() {
-
+        Euro a= new Euro(1,20);
+        assertEquals("1.2 euro" , a.stampa());
     }
 
     // TODO(anubi0)
