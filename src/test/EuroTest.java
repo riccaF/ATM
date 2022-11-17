c4c00a9438d2c4c63d1c1771afebd2871e836f49
federico conti <federicoconti1702@gmail.com>
2022-11-17 12:15:21 +0100

    Merge Feature-->Test e Inizializzto JUnit

---
+++ src/test/EuroTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.junit.Test;
+
+public class EuroTest {
+    @Test
+    public void testGetValore() {
+
+    }
+
+    @Test
+    public void testMinoreDi() {
+
+    }
+
+    @Test
+    public void testSomma() {
+
+    }
+
+    @Test
+    public void testSottrai() {
+
+    }
+
+    @Test
+    public void testStampa() {
+
+    }
+
+    @Test
+    public void testUgualeA() {
+
+    }
+}
