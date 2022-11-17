a47faefc91f126f4f527026a0b1551df1c4ff54b
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2022-11-17 14:18:42 +0100

    added testGetValore and testSottrai

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -1,12 +1,15 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import code.Business_logic.Euro;
 
 public class TestEuro {
     // TODO(arkeane)
     @Test
     public void testGetValore() {
-
+        Euro e = new Euro(10, 50);
+        assertEquals(1050, e.getValore());
     }
 
     // TODO(iobah)
@@ -24,7 +27,10 @@ public class TestEuro {
     // TODO(arkeane)
     @Test
     public void testSottrai() {
-
+        Euro e1 = new Euro(2, 50);
+        Euro e2 = new Euro(5, 50);
+        e1.sottrai(e2);
+        assertEquals(-300, e1.getValore());
     }
 
     // TODO(iobah)
