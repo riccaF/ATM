2de18c51951c8cbc870bc6bd297b125a74cb6ac4
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-11-17 16:42:43 +0100

    implementati "testGetAccountNumber", "testGetAvailableBalance" e "testCredit"

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,6 +1,11 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+import code.Business_logic.Account;
+import code.Business_logic.Euro;
 
 public class AccountTest {
     @Test
@@ -15,12 +20,19 @@ public class AccountTest {
 
     @Test
     public void testGetAccountNumber() {
+        Account acc = new Account(12345, 54321, new Euro(10.0), new Euro(100.0));
+        int expectedAccountNumber = 12345;
 
+        assertEquals(expectedAccountNumber, acc.getAccountNumber());
     }
 
     @Test
     public void testGetAvailableBalance() {
+        Account acc = new Account(12345, 54321, new Euro(12345.0), new Euro(1000000.0));
+        Euro expectedBalance = new Euro(12345.0);
 
+        // assertTrue(expectedBalance.ugualeA(acc.getAvailableBalance()));
+        assertEquals(expectedBalance.getValore(), acc.getAvailableBalance().getValore()); // getValore() è già stato testato in EuroTest.java <3
     }
 
     @Test
--- src/test/BankDatabaseTest.java
+++ src/test/BankDatabaseTest.java
@@ -1,16 +1,27 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import code.Business_logic.Account;
+import code.Business_logic.Euro;
 
 public class BankDatabaseTest {
     @Test
     public void testAuthenticateUser() {
+        
 
     }
 
     @Test
     public void testCredit() {
+        Account acc = new Account(12345, 54321, new Euro(100.0), new Euro(1000));
+        Euro expectedBalance = new Euro(1500);
 
+        acc.credit(500.0);
+
+        assertEquals(expectedBalance.getValore(), acc.getTotalBalance().getValore());
     }
 
     @Test
