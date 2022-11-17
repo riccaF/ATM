ecd9d790f895b511f8b13285a8052a0b18cadb43
ludof63 <f63capia@gmail.com>
2022-11-17 14:16:32 +0100

    adding Account tests (empty)

---
+++ src/test/AccountTest.java
@@ -0,0 +1,34 @@
+package test;
+import org.junit.Test;
+
+public class AccountTest {
+    @Test
+    public void testCredit() {
+
+    }
+
+    @Test
+    public void testDebit() {
+
+    }
+
+    @Test
+    public void testGetAccountNumber() {
+
+    }
+
+    @Test
+    public void testGetAvailableBalance() {
+
+    }
+
+    @Test
+    public void testGetTotalBalance() {
+
+    }
+
+    @Test
+    public void testValidatePIN() {
+
+    }
+}
