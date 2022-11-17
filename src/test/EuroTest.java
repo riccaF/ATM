782feffbe31c6a6e0f725213c8cb72cd7bbafec6
Christian Parodi <christian.parodi19@gmail.com>
2022-11-17 12:35:17 +0100

    Implementati i test testStampa e testUgualeA

--- src/test/EuroTest.java
+++ src/test/EuroTest.java
@@ -1,14 +1,16 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 
-import code.Business_logic.Euro; 
+import code.Business_logic.Euro;
 
 public class EuroTest {
 
     @Test
     public void testGetValore() {
-       
+
     }
 
     @Test
@@ -28,11 +30,16 @@ public class EuroTest {
 
     @Test
     public void testStampa() {
-
+        Euro eu = new Euro(1000, 50);
+        String actual = eu.stampa();
+        String expected = "1000.5 euro";
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testUgualeA() {
-
+        Euro eu = new Euro(1000, 50);
+        long expected = eu.getValore();
+        assertEquals(expected, 100050);
     }
 }
