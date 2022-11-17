f3f3ddac803198bec037e70d490bacbcf2284411
federico conti <federicoconti1702@gmail.com>
2022-11-17 12:37:54 +0100

    Implementati testMinoreDi e testGetValore in EuroTest

--- src/test/EuroTest.java
+++ src/test/EuroTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import code.Business_logic.Euro; 
@@ -8,12 +11,17 @@ public class EuroTest {
 
     @Test
     public void testGetValore() {
-       
+       Euro eu = new Euro(125,50);
+       long exptected = 12550;
+       assertEquals(exptected, eu.getValore());
     }
 
     @Test
     public void testMinoreDi() {
-
+        Euro eu1 = new Euro(125,50);
+        Euro eu2 = new Euro(15,70);
+        boolean exptected = false;
+        assertEquals(exptected, eu1.minoreDi(eu2));
     }
 
     @Test
