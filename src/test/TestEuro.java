373166777e5b0d480f19bed82c1c3345a5758527
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2022-11-17 12:43:47 +0100

    divided tests between group members

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -3,31 +3,37 @@ package test;
 import org.junit.Test;
 
 public class TestEuro {
+    // TODO(arkeane)
     @Test
     public void testGetValore() {
 
     }
 
+    // TODO(iobah)
     @Test
     public void testMinoreDi() {
 
     }
 
+    // TODO(anubi0)
     @Test
     public void testSomma() {
 
     }
 
+    // TODO(arkeane)
     @Test
     public void testSottrai() {
 
     }
 
+    // TODO(iobah)
     @Test
     public void testStampa() {
 
     }
 
+    // TODO(anubi0)
     @Test
     public void testUgualeA() {
 
