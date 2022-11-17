408c6e9b4bbc5e2133cadb34e2c169fa225bb9b2
Christian Parodi <christian.parodi19@gmail.com>
2022-11-17 16:44:31 +0100

    Implementati AccountTest.testGetTotalBalance, AccountTest.testValidatePin, BankDatabaseTest.testGetTotalBalance

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,6 +1,12 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import code.Business_logic.Account;
+import code.Business_logic.Euro;
 
 public class AccountTest {
     @Test
@@ -25,11 +31,16 @@ public class AccountTest {
 
     @Test
     public void testGetTotalBalance() {
-
+        Account acc = new Account(1, 1, new Euro(10), new Euro(100));
+        long actual = acc.getTotalBalance().getValore();
+        long expected = 10000;
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testValidatePIN() {
-
+        int pin = 1;
+        Account acc = new Account(1, pin, new Euro(10), new Euro(100));
+        assertTrue(acc.validatePIN(pin));
     }
 }
--- src/test/BankDatabaseTest.java
+++ src/test/BankDatabaseTest.java
@@ -1,6 +1,12 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import code.Business_logic.Account;
+import code.Business_logic.Euro;
+import code.Database.BankDatabase;
 
 public class BankDatabaseTest {
     @Test
@@ -25,6 +31,9 @@ public class BankDatabaseTest {
 
     @Test
     public void testGetTotalBalance() {
-
+        BankDatabase db = new BankDatabase();
+        Euro actual = db.getTotalBalance(12345);
+        Euro expected = new Account(12345, 54321, new Euro(1000.0), new Euro(1200.0)).getTotalBalance();
+        assertEquals(expected.getValore(), actual.getValore());
     }
 }
