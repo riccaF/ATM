520491bbf0fedee91f7b48debbda1ed9625eff7e
federico conti <federicoconti1702@gmail.com>
2022-11-17 16:47:00 +0100

    Implementato testCredit e testDebit

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -2,15 +2,31 @@ package test;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import code.Business_logic.Euro;
+import code.Business_logic.Account;
+
 public class AccountTest {
     @Test
     public void testCredit() {
-
+        Euro available = new Euro(1000, 0);
+        Euro total = new Euro(1500, 0);
+        Account acc = new Account(12345, 54321, available, total);
+        acc.credit(200);
+        Euro expectedTotal = new Euro(1700, 0);
+        assertEquals(expectedTotal.getValore(), acc.getTotalBalance().getValore());
     }
 
     @Test
     public void testDebit() {
-
+        Euro available = new Euro(1000, 0);
+        Euro total = new Euro(1500, 0);
+        Account acc = new Account(12345, 54321, available, total);
+        acc.debit(200);
+        Euro expectedAvailable = new Euro(800, 0);
+        Euro expectedTotal = new Euro(1300, 0);
+        assertEquals(expectedAvailable.getValore(), acc.getAvailableBalance().getValore());
+        assertEquals(expectedTotal.getValore(), acc.getTotalBalance().getValore());
     }
 
     @Test
