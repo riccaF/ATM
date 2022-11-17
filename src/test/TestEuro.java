de007c61fc335558e5b9dd5bb5652b077c6f67da
SabrinaPolidoro <sabrina_polidoro@hotmail.it>
2022-11-17 17:54:34 +0100

    updated testEuro (SetUp method and other assert)

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -1,52 +1,63 @@
 package test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import code.Business_logic.Euro;
 
 public class TestEuro {
    
+    Euro testEuro, testEuro1, testEuro2, testEuro3, testEuro4, testEuro5;
+
+    @Before
+    public void SetUp(){
+        testEuro = new Euro(1,23);
+        testEuro1 = new Euro(100,23);
+        testEuro2 = new Euro(90,23);
+        testEuro3 = new Euro(3);
+        testEuro4 = new Euro(3,0);
+        testEuro5 = new Euro(2,99);
+    }
+
 
     @Test
     public void testGetValore() {
-        var testEuro = new Euro(1,23);
         assertEquals(123, testEuro.getValore());
+        assertNotEquals("1,23", testEuro.getValore());
     }
 
     @Test
     public void testMinoreDi() {
-        var testEuro1 = new Euro(99,23);
-        var testEuro2 = new Euro(100,23);
-        assertTrue("99.23 < 100.23", testEuro1.minoreDi(testEuro2));     
+        assertTrue("90.23 < 100.23", testEuro2.minoreDi(testEuro1));
+        assertFalse("100.23 < 900.23", !(testEuro2.minoreDi(testEuro1)));
     }
 
     @Test
     public void testSomma() {
-        var testEuro1 = new Euro(3,20);
-        var testEuro2 = new Euro(2,20);
-        assertEquals(540, testEuro1.somma(testEuro2).getValore());
+        assertEquals(19046, testEuro1.somma(testEuro2).getValore());
+        assertNotEquals("190,46", testEuro1.somma(testEuro2).getValore());
     }
 
     @Test
     public void testSottrai() {
-        var testEuro1 = new Euro(3,20);
-        var testEuro2 = new Euro(2,20);
-        assertEquals(100, testEuro1.sottrai(testEuro2).getValore());
+        assertEquals(9900, testEuro1.sottrai(testEuro).getValore());
+        assertNotEquals(9900, testEuro2.sottrai(testEuro1).getValore());
     }
 
     @Test
     public void testStampa() {
-        var testEuro1 = new Euro(3);
-        assertEquals("3.0 euro", testEuro1.stampa());
+        assertEquals("1.23 euro", testEuro.stampa());
+        assertNotEquals(123, testEuro.stampa());
 
     }
 
     @Test
     public void testUgualeA() {
-        var testEuro1 = new Euro(3);
-        var testEuro2 = new Euro(3,0);
-        assertTrue("3 euro equivalgono a 3.0 euro", testEuro1.ugualeA(testEuro2));
+        assertTrue("3 euro equivalgono a 3.0 euro", testEuro4.ugualeA(testEuro3));
+        assertFalse("3 euro NON equivalgono a 2.99 euro", testEuro4.ugualeA(testEuro5));
     }
 }
