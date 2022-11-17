403091f4c084d54b4d4db6183f5ca8113ecf21cf
ludof63 <f63capia@gmail.com>
2022-11-17 12:04:19 +0100

    adding euro test class

---
+++ src/TestEuro.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+public class TestEuro {
+    @Test
+    public void testGetValore() {
+
+    }
+
+    @Test
+    public void testMinoreDi() {
+
+    }
+
+    @Test
+    public void testSomma() {
+
+    }
+
+    @Test
+    public void testSottrai() {
+
+    }
+
+    @Test
+    public void testStampa() {
+
+    }
+
+    @Test
+    public void testUgualeA() {
+
+    }
+}
