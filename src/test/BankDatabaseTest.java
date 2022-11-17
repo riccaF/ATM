81412b192817ff2bb3f16bb22b35b28cc924d7ce
federico conti <federicoconti1702@gmail.com>
2022-11-17 16:08:06 +0100

    Aggiunto BanckDatabaseTest e AccountTest

---
+++ src/test/BankDatabaseTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.junit.Test;
+
+public class BankDatabaseTest {
+    @Test
+    public void testAuthenticateUser() {
+
+    }
+
+    @Test
+    public void testCredit() {
+
+    }
+
+    @Test
+    public void testDebit() {
+
+    }
+
+    @Test
+    public void testGetAvailableBalance() {
+
+    }
+
+    @Test
+    public void testGetTotalBalance() {
+
+    }
+}
