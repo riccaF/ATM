3f6ca128e2d0330852776ba3cd9a4ca65f99674e
SabrinaPolidoro <sabrina_polidoro@hotmail.it>
2022-11-17 12:48:07 +0100

    TestEuro with testGetValore and testMinoreDi

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -1,15 +1,28 @@
 package test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
+import code.Business_logic.Euro;
+
 public class TestEuro {
+   
+
     @Test
     public void testGetValore() {
-
+        double d = 0.9 ;
+        var testEuro = new Euro(d);
+        assertEquals(90, testEuro.getValore());
     }
 
     @Test
     public void testMinoreDi() {
-
+        double d1 = 0.9 ;
+        var testEuro1 = new Euro(d1);
+        double d2 = 0.7 ;
+        var testEuro2 = new Euro(d2);
+        assertTrue("70€ < 90€", testEuro2.minoreDi(testEuro1));       
     }
 
     @Test
