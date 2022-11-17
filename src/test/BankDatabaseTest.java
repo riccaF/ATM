62d0ec494f8542e0768679efee1c2f25ed78f341
ludof63 <f63capia@gmail.com>
2022-11-17 18:52:42 +0100

    test BankDatabase: adding test AuthenticateUser

--- src/test/BankDatabaseTest.java
+++ src/test/BankDatabaseTest.java
@@ -1,11 +1,31 @@
 package test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 
-public class BankDatabaseTest {
-    @Test
-    public void testAuthenticateUser() {
+import code.Database.BankDatabase;
 
+public class BankDatabaseTest {
+
+    BankDatabase bdtest;
+
+    @Before
+    public void SetUp() {
+        bdtest = new BankDatabase();
     }
+
+    @Test
+    public void testValidAuthenticateUser() {
+        assertTrue("User 1234 is authenticated", bdtest.authenticateUser(12345, 54321));
+    }
+
+    @Test
+    public void testInvalidAuthenticateUser() {
+        assertTrue("User 1234 is not authenticated", !bdtest.authenticateUser(1234, 5432));
+    }
+
 
     @Test
     public void testCredit() {
