e7624c5d263696c5417b3dd5b47b04e99bdfbc17
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-11-17 12:37:47 +0100

    implementati "testSomma" "testSottrai" <3

--- src/test/EuroTest.java
+++ src/test/EuroTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import code.Business_logic.Euro; 
@@ -18,12 +20,20 @@ public class EuroTest {
 
     @Test
     public void testSomma() {
+        Euro eu = new Euro(100,55);
+        Euro toSum = new Euro(10,45);
+        Euro expected = new Euro(111,00);
 
+        assertEquals(expected.getValore(), eu.somma(toSum).getValore());
     }
 
     @Test
     public void testSottrai() {
+        Euro eu = new Euro(100,55);
+        Euro toSub = new Euro(10,55);
+        Euro expected = new Euro(90,00);
 
+        assertEquals(expected.getValore(), eu.sottrai(toSub).getValore());
     }
 
     @Test
