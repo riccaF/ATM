013e9b5bf81d212c0db964bd286a162ac3f24733
ludof63 <f63capia@gmail.com>
2022-11-17 14:16:18 +0100

    adding BankDatabase tests (empty)

---
+++ src/test/BankDatabaseTest.java
@@ -0,0 +1,29 @@
+package test;
+import org.junit.Test;
+
+public class BankDatabaseTest {
+    @Test
+    public void testAuthenticateUser() {
+
+    }
+
+    @Test
+    public void testCredit() {
+
+    }
+
+    @Test
+    public void testDebit() {
+
+    }
+
+    @Test
+    public void testGetAvailableBalance() {
+
+    }
+
+    @Test
+    public void testGetTotalBalance() {
+
+    }
+}
