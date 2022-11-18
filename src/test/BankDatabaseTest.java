ae9c263ee6beaefcebfd877a27fed49a5a2d8938
Luca <s4844974@studenti.unige.it>
2022-11-18 12:35:03 +0100

    completed all BankDatabase Tests

--- src/test/BankDatabaseTest.java
+++ src/test/BankDatabaseTest.java
@@ -1,9 +1,11 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import code.Business_logic.Euro;
 import code.Database.BankDatabase;
 
 public class BankDatabaseTest {
@@ -15,21 +17,29 @@ public class BankDatabaseTest {
 
     @Test
     public void testCredit() {
-
+        BankDatabase bankDatabase = new BankDatabase();
+        bankDatabase.credit(12345, new Euro(100));
+        assertEquals(130000,bankDatabase.getTotalBalance(12345).getValore());
     }
 
     @Test
     public void testDebit() {
+        BankDatabase bankDatabase = new BankDatabase();
+        bankDatabase.debit(12345, new Euro(100));
+        assertEquals(110000,bankDatabase.getTotalBalance(12345).getValore());
 
     }
 
     @Test
     public void testGetAvailableBalance() {
-
+        BankDatabase bankDatabase = new BankDatabase();
+        assertEquals(100000,bankDatabase.getAvailableBalance(12345).getValore());
     }
 
     @Test
     public void testGetTotalBalance() {
+        BankDatabase bankDatabase = new BankDatabase();
+        assertEquals(120000,bankDatabase.getTotalBalance(12345).getValore());
 
     }
 }
