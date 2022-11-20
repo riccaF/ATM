388b2fcc9bb49811691f558cfc9e54dd424d8f8e
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-11-20 21:22:54 +0100

    ATM 2.0 - utlimo commit, merge all

--- src/test/BankDatabaseTest.java
+++ src/test/BankDatabaseTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
+import org.junit.Before; 
 import org.junit.Test;
 
 import code.Business_logic.Account;
