6755ff2d1de2d17db53befca1d8e86ad6db7f429
federico conti <federicoconti1702@gmail.com>
2022-11-20 19:19:20 +0100

    Implementato testAuthenticateUser e Aggiunto @Before setUp

--- src/test/BankDatabaseTest.java
+++ src/test/BankDatabaseTest.java
@@ -1,7 +1,10 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import code.Business_logic.Account;
@@ -9,34 +12,40 @@ import code.Business_logic.Euro;
 import code.Database.BankDatabase;
 
 public class BankDatabaseTest {
+
+    BankDatabase db;
+
+    @Before
+    public void setUp() {
+        db = new BankDatabase();
+    }
+
     @Test
     public void testAuthenticateUser() {
-
+        assertFalse(db.authenticateUser(12348, 54326));
+        assertTrue(db.authenticateUser(98765, 56789));
     }
 
     @Test
     public void testCredit() {
-        BankDatabase db = new BankDatabase();
         Euro expectedBalance = new Euro(1700);
 
-        db.credit(12345,500.0);
+        db.credit(12345, 500.0);
 
         assertEquals(expectedBalance.getValore(), db.getTotalBalance(12345).getValore());
     }
 
     @Test
     public void testDebit() {
-        BankDatabase db = new BankDatabase();
         Euro expectedBalance = new Euro(700);
 
-        db.debit(12345,500.0);
+        db.debit(12345, 500.0);
 
         assertEquals(expectedBalance.getValore(), db.getTotalBalance(12345).getValore());
     }
 
     @Test
     public void testGetAvailableBalance() {
-        BankDatabase db = new BankDatabase();
         Euro actual = db.getAvailableBalance(12345);
         Euro expected = new Account(12345, 54321, new Euro(1000.0), new Euro(1200.0)).getAvailableBalance();
         assertEquals(expected.getValore(), actual.getValore());
@@ -44,7 +53,6 @@ public class BankDatabaseTest {
 
     @Test
     public void testGetTotalBalance() {
-        BankDatabase db = new BankDatabase();
         Euro actual = db.getTotalBalance(12345);
         Euro expected = new Account(12345, 54321, new Euro(1000.0), new Euro(1200.0)).getTotalBalance();
         assertEquals(expected.getValore(), actual.getValore());
