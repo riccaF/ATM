7e800316e62431ea1039c61c5c79bf58063699ac
s5003659 <traversa.riccardo14@gmail.com>
2023-11-21 16:02:40 +0100

    Creazione package GUI, Database, Business_logic

--- src/code/GUI/CashDispenser.java
+++ src/code/GUI/CashDispenser.java
@@ -1,4 +1,4 @@
-package code;
+package code.GUI;
 // CashDispenser.java
 // Represents the cash dispenser of the ATM
 
--- src/code/GUI/DepositSlot.java
+++ src/code/GUI/DepositSlot.java
@@ -1,27 +1,16 @@
-package code;
-// Screen.java
-// Represents the screen of the ATM
+package code.GUI;
+// DepositSlot.java
+// Represents the deposit slot of the ATM
 
-public class Screen
+public class DepositSlot 
 {
-   // displays a message without a carriage return
-   public void displayMessage( String message ) 
+   // indicates whether envelope was received (always returns true, 
+   // because this is only a software simulation of a real deposit slot)
+   public boolean isEnvelopeReceived()
    {
-      System.out.print( message ); 
-   } // end method displayMessage
-
-   // display a message with a carriage return
-   public void displayMessageLine( String message ) 
-   {
-      System.out.println( message );   
-   } // end method displayMessageLine
-
-   // display a dollar amount
-   public void displayDollarAmount( double amount )
-   {
-      System.out.printf( "$%,.2f", amount );   
-   } // end method displayDollarAmount 
-} // end class Screen
+      return true; // deposit envelope was received
+   } // end method isEnvelopeReceived
+} // end class DepositSlot
 
 
 
