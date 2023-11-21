fbfb97f81a3d4741c6ffaf72264082c35f384255
mattia_diluca <diluca.mattia02@gmail.com>
2023-11-21 12:27:14 +0100

    modifica BalanceInquiry

--- src/code/BalanceInquiry.java
+++ src/code/BalanceInquiry.java
@@ -20,19 +20,19 @@ public class BalanceInquiry extends Transaction
       Screen screen = getScreen();
 
       // get the available balance for the account involved
-      double availableBalance = 
+      Euro availableBalance = 
          bankDatabase.getAvailableBalance( getAccountNumber() );
 
       // get the total balance for the account involved
-      double totalBalance = 
+      Euro totalBalance = 
          bankDatabase.getTotalBalance( getAccountNumber() );
       
       // display the balance information on the screen
       screen.displayMessageLine( "\nBalance Information:" );
       screen.displayMessage( " - Available balance: " ); 
-      screen.displayDollarAmount( availableBalance );
+      screen.displayEuroAmount( availableBalance );
       screen.displayMessage( "\n - Total balance:     " );
-      screen.displayDollarAmount( totalBalance );
+      screen.displayEuroAmount( totalBalance );
       screen.displayMessageLine( "" );
    } // end method execute
 } // end class BalanceInquiry
