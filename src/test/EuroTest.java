12a8ff7cb07ca89d0e3e43179553380505f42dc2
Arun Mathiyalakan <friday99@outlook.it>
2023-11-25 16:12:45 +0100

    ADD: testSomma, testSottrai & testUgualeA

--- src/test/EuroTest.java
+++ src/test/EuroTest.java
@@ -7,22 +7,50 @@ import code.Business_logic.Euro;
 
 public class EuroTest {
 
-    @Test
-    public void testCostruttoreConDueParametriPositivi() {
-        Euro euro = new Euro(5, 50);
-        assertEquals(550, euro.getValore());
-    }
+  @Test
+  public void testCostruttoreConDueParametriPositivi() {
+    Euro euro = new Euro(5, 50);
+    assertEquals(550, euro.getValore());
+  }
 
-    @Test
-    public void testCostruttoreConDueParametriDiCuiEuroNegativo() {
-        Euro euro = new Euro(-5, 50);
-        assertEquals(-550, euro.getValore());
-    }
+  @Test
+  public void testCostruttoreConDueParametriDiCuiEuroNegativo() {
+    Euro euro = new Euro(-5, 50);
+    assertEquals(-550, euro.getValore());
+  }
 
-    @Test
-    public void testCostruttoreConSingoloParamentro() {
-        Euro euro = new Euro(5.50);
-        assertEquals(550, euro.getValore());
-    }
+  @Test
+  public void testCostruttoreConSingoloParametro() {
+    Euro euro = new Euro(5.50);
+    assertEquals(550, euro.getValore());
+  }
+
+  @Test
+  public void testSomma() {
+    Euro euro1 = new Euro(5, 50);
+    Euro euro2 = new Euro(3, 75);
+
+    Euro risultato = euro1.somma(euro2);
+    assertEquals(925, risultato.getValore());
+  }
+
+  @Test
+  public void testSottrai() {
+    Euro euro1 = new Euro(5, 50);
+    Euro euro2 = new Euro(3, 75);
+
+    Euro risultato = euro1.sottrai(euro2);
+    assertEquals(175, risultato.getValore());
+  }
+
+  @Test
+  public void testUgualeA() {
+    Euro euro1 = new Euro(5, 50);
+    Euro euro2 = new Euro(5, 50);
+    assertTrue(euro1.ugualeA(euro2));
+
+    Euro euro3 = new Euro(8, 25);
+    assertFalse(euro1.ugualeA(euro3));
+  }
 
 }
