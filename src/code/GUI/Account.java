d394704a42c28c394ad3ef9a9f085d17fad10514
Iosif Puha <iosifpuha@yahoo.com>
2023-11-25 17:50:15 +0100

    Prima prova, già testato in locale, alcune feature non vanno come si deve

--- src/code/GUI/Account.java
+++ src/code/GUI/Account.java
@@ -1,17 +1,16 @@
 package code.GUI;// code.GUI.Account.java
+
 // Represents a bank account
 
-public class Account 
-{
+public class Account {
    private int accountNumber; // account number
    private int pin; // PIN for authentication
-   private double availableBalance; // funds available for withdrawal
-   private double totalBalance; // funds available + pending deposits
+   private Euro availableBalance; // funds available for withdrawal
+   private Euro totalBalance; // funds available + pending deposits
 
    // code.GUI.Account constructor initializes attributes
-   public Account( int theAccountNumber, int thePIN, 
-      double theAvailableBalance, double theTotalBalance )
-   {
+   public Account(int theAccountNumber, int thePIN,
+         Euro theAvailableBalance, Euro theTotalBalance) {
       accountNumber = theAccountNumber;
       pin = thePIN;
       availableBalance = theAvailableBalance;
@@ -19,58 +18,53 @@ public class Account
    } // end code.GUI.Account constructor
 
    // determines whether a user-specified PIN matches PIN in code.GUI.Account
-   public boolean validatePIN( int userPIN )
-   {
-      if ( userPIN == pin )
+   public boolean validatePIN(int userPIN) {
+      if (userPIN == pin)
          return true;
       else
          return false;
    } // end method validatePIN
-   
+
    // returns available balance
-   public double getAvailableBalance()
-   {
+   public Euro getAvailableBalance() {
       return availableBalance;
    } // end getAvailableBalance
 
    // returns the total balance
-   public double getTotalBalance()
-   {
+   public Euro getTotalBalance() {
       return totalBalance;
    } // end method getTotalBalance
 
    // credits an amount to the account
-   public void credit( double amount )
-   {
-      totalBalance += amount; // add to total balance
+   public void credit(Euro amount) {
+      amount.somma(totalBalance); // add to total balance
    } // end method credit
 
    // debits an amount from the account
-   public void debit( double amount )
-   {
-      availableBalance -= amount; // subtract from available balance
-      totalBalance -= amount; // subtract from total balance
+   public void debit(Euro amount) {
+      // subtract from available balance
+      availableBalance.sottrai(amount);
+      // subtract from total balance
+      totalBalance.sottrai(amount);
    } // end method debit
 
    // returns account number
-   public int getAccountNumber()
-   {
-      return accountNumber;  
+   public int getAccountNumber() {
+      return accountNumber;
    } // end method getAccountNumber
 } // end class code.GUI.Account
 
-
 /**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
+ * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and *
+ * Pearson Education, Inc. All Rights Reserved. *
+ * *
+ * DISCLAIMER: The authors and publisher of this book have used their *
+ * best efforts in preparing the book. These efforts include the *
+ * development, research, and testing of the theories and programs *
+ * to determine their effectiveness. The authors and publisher make *
+ * no warranty of any kind, expressed or implied, with regard to these *
  * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * and publisher shall not be liable in any event for incidental or *
+ * consequential damages in connection with, or arising out of, the *
+ * furnishing, performance, or use of these programs. *
  *************************************************************************/
--- src/code/GUI/Deposit.java
+++ src/code/GUI/Deposit.java
@@ -1,23 +1,22 @@
 package code.GUI;// code.GUI.Deposit.java
+
 // Represents a deposit code.GUI.ATM transaction
 
 import code.Database.BankDatabase;
 import code.Business_logic.Transaction;
 
-public class Deposit extends Transaction
-{
-   private double amount; // amount to deposit
+public class Deposit extends Transaction {
+   private Euro amount; // amount to deposit
    private Keypad keypad; // reference to keypad
    private DepositSlot depositSlot; // reference to deposit slot
    private final static int CANCELED = 0; // constant for cancel option
 
    // code.GUI.Deposit constructor
    public Deposit(int userAccountNumber, Screen atmScreen,
-                  BankDatabase atmBankDatabase, Keypad atmKeypad,
-                  DepositSlot atmDepositSlot )
-   {
+         BankDatabase atmBankDatabase, Keypad atmKeypad,
+         DepositSlot atmDepositSlot) {
       // initialize superclass variables
-      super( userAccountNumber, atmScreen, atmBankDatabase );
+      super(userAccountNumber, atmScreen, atmBankDatabase);
 
       // initialize references to keypad and deposit slot
       keypad = atmKeypad;
@@ -25,81 +24,77 @@ public class Deposit extends Transaction
    } // end code.GUI.Deposit constructor
 
    // perform transaction
-   public void execute()
-   {
+   public void execute() {
       BankDatabase bankDatabase = getBankDatabase(); // get reference
       Screen screen = getScreen(); // get reference
 
       amount = promptForDepositAmount(); // get deposit amount from user
 
       // check whether user entered a deposit amount or canceled
-      if ( amount != CANCELED )
-      {
+      if (amount.getValore() != CANCELED) {
          // request deposit envelope containing specified amount
-         screen.displayMessage( 
-            "\nPlease insert a deposit envelope containing " );
-         screen.displayDollarAmount( amount );
-         screen.displayMessageLine( "." );
+         screen.displayMessage(
+               "\nPlease insert a deposit envelope containing ");
+         screen.displayEuroAmount(amount);
+         screen.displayMessageLine(".");
 
          // receive deposit envelope
          boolean envelopeReceived = depositSlot.isEnvelopeReceived();
 
          // check whether deposit envelope was received
-         if ( envelopeReceived )
-         {  
-            screen.displayMessageLine( "\nYour envelope has been " + 
-               "received.\nNOTE: The money just deposited will not " + 
-               "be available until we verify the amount of any " +
-               "enclosed cash and your checks clear." );
-            
+         if (envelopeReceived) {
+            screen.displayMessageLine("\nYour envelope has been " +
+                  "received.\nNOTE: The money just deposited will not " +
+                  "be available until we verify the amount of any " +
+                  "enclosed cash and your checks clear.");
+
             // credit account to reflect the deposit
-            bankDatabase.credit( getAccountNumber(), amount ); 
+            bankDatabase.credit(getAccountNumber(), amount);
          } // end if
          else // deposit envelope not received
          {
-            screen.displayMessageLine( "\nYou did not insert an " +
-               "envelope, so the code.GUI.ATM has canceled your transaction." );
+            screen.displayMessageLine("\nYou did not insert an " +
+                  "envelope, so the code.GUI.ATM has canceled your transaction.");
          } // end else
-      } // end if 
+      } // end if
       else // user canceled instead of entering amount
       {
-         screen.displayMessageLine( "\nCanceling transaction..." );
+         screen.displayMessageLine("\nCanceling transaction...");
       } // end else
    } // end method execute
 
-   // prompt user to enter a deposit amount in cents 
-   private double promptForDepositAmount()
-   {
+   // prompt user to enter a deposit amount in cents
+   private Euro promptForDepositAmount() {
       Screen screen = getScreen(); // get reference to screen
 
       // display the prompt
-      screen.displayMessage( "\nPlease enter a deposit amount in " + 
-         "CENTS (or 0 to cancel): " );
+      screen.displayMessage("\nPlease enter a deposit amount in " +
+            "CENTS (or 0 to cancel): ");
       int input = keypad.getInput(); // receive input of deposit amount
-      
+
       // check whether the user canceled or entered a valid amount
-      if ( input == CANCELED ) 
-         return CANCELED;
-      else
-      {
-         return ( double ) input / 100; // return dollar amount 
+      if (input == CANCELED)
+         return new Euro(0); // non so se sia la cosa più intelligente da fare ma è stato l'unico modo che mi
+                             // è venuto in mente per uscirne
+      else {
+         return new Euro((double) input / 100); // return Euro amount, lui faceva: (double) input / 100;
+         // return an Euro object
+         // return new Euro((double) input / 100);
       } // end else
    } // end method promptForDepositAmount
 } // end class code.GUI.Deposit
 
-
-
 /**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
+ * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and *
+ * Pearson Education, Inc. All Rights Reserved. *
+ * *
+ * DISCLAIMER: The authors and publisher of this book have used their *
+ * best efforts in preparing the book. These efforts include the *
+ * development, research, and testing of the theories and programs *
+ * to determine their effectiveness. The authors and publisher make *
+ * no warranty of any kind, expressed or implied, with regard to these *
  * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * and publisher shall not be liable in any event for incidental or *
+ * consequential damages in connection with, or arising out of, the *
+ * furnishing, performance, or use of these programs. *
  *************************************************************************/
