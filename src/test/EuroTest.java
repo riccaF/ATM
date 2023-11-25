fded15b41f96115b4278c98a6467da59d83a9351
Arun Mathiyalakan <friday99@outlook.it>
2023-11-25 19:06:35 +0100

    UPDATE: ready for conflict #4

--- src/test/EuroTest.java
+++ src/test/EuroTest.java
@@ -63,7 +63,7 @@ public class EuroTest {
   }
 
   @Test
-  public void testStampaaaaaaaaaaaaa() {
+  public void testStampa() {
     Euro euro = new Euro(5, 50);
     assertEquals("5.5 euro", euro.stampa());
   }
