b20f5ca58549e16b309eb2c31c979a807090fcbf
martayay <scarsimarta02@gmail.com>
2023-11-26 22:30:01 +0100

    aggiunti nuovi test

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -9,21 +9,30 @@ public class TestEuro {
     
     private Euro euro1;
     private Euro euro2;
+    private Euro euro3;
+    private Euro euro4;
     
     @Before  //usiamo @Before e non @BeforeEach perchè usiamo junit 4
     public void setUp() {
         euro1 = new Euro(10, 50);
         euro2 = new Euro(15, 25);
+        euro3 = new Euro(20, 00);
+        euro4 = new Euro(30, 00);
+
     }
 
     @Test
     public void testGetValore() {
         
         long expectedValore = 1050;
-        
         long actualValore = euro1.getValore();
-        
+
+        long expectedValore1 = 2000;
+        long actualValore1 = euro3.getValore();
+
         assertEquals(expectedValore, actualValore);
+        assertEquals(expectedValore1, actualValore1);
+        
     }
 
     @Test
@@ -31,6 +40,9 @@ public class TestEuro {
         
         assertTrue(euro1.minoreDi(euro2));
         assertFalse(euro2.minoreDi(euro1));
+        assertTrue(euro3.minoreDi(euro4));
+        assertFalse(euro4.minoreDi(euro3));
+        assertTrue(euro2.minoreDi(euro3));
     }
 
     @Test
