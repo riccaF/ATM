7a7f481989d4e7c8392a26a35f178bb84cd619f1
LouchanLouchan <maurasimiras@gmail.com>
2023-11-27 10:56:34 +0100

    Aggiunta branch test+ classi Test

---
+++ src/code/test/TestAccount.java
@@ -0,0 +1,41 @@
+package code.test;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import code.Business_logic.Account;
+import code.Business_logic.Euro;
+
+public class TestAccount {
+    //i test valgono anche per il Constructor B1, B2, B3, B4
+    @Test
+    public void testConstructorA1(){
+        Account user= new Account(000001, 1234, 100.0, 100.0);
+        assertEquals(000001, user.getAccountNumber());
+    
+    }
+    @Test
+    public void testConstructorA3(){
+        Account user= new Account(000001, 1234, 100.0, 100.0);
+        assertEquals(100.0, user.getAvailableBalance());
+    }
+    @Test
+    public void testConstructorA4(){
+        Account user= new Account(000001, 1234, 100.0, 100.0);
+        assertEquals(100.0, user.getTotalBalance());
+    }
+    @Test
+    public void TestCredit(){
+        Account account = new Account(0001, 1234, 100.0, 100.0);
+        Euro totalBalance = account.getTotalBalance();
+        assertEquals(50.25, totalBalance.getValoreD(), 0.01);
+    }
+    
+    @Test
+    public void testDebit() {
+        Account account = new Account(0001, 1234, 100.0, 100.0);
+        Euro amount = new Euro(50, 25);
+        account.debit(amount);
+        Euro totalBalance = account.getTotalBalance();
+        assertEquals(49.75, totalBalance.getValoreD(), 0.01);
+    }
+}
---
+++ src/code/test/TestEuro.java
@@ -0,0 +1,93 @@
+package code.test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import code.Business_logic.Euro;
+
+public class TestEuro {
+    @Test
+    public void testConstructorA1(){
+        Euro element = new Euro(1000, 30);
+        assertEquals(100030,element.getValore());
+    }
+
+    @Test
+    public void testConstructorA2(){
+        Euro element = new Euro(-1000, 30);
+        assertEquals(-100030,element.getValore());    
+    }
+
+    @Test
+    public void testConstructorB1(){
+        Euro element = new Euro(12.34);
+        assertEquals(1234,element.getValore());
+    }
+
+    @Test
+    public void testSomma(){
+        double num1 = 56789.23;
+        double num2 = 12345.67;
+        Euro a = new Euro(num1);
+        Euro b = new Euro(num2);
+        a.somma(b);
+        assertEquals((long)((num1+num2)*100),a.getValore());
+    }
+
+    @Test
+    public void testSottrai(){
+        double num1 = 56789.23;
+        double num2 = 12345.67;
+        Euro a = new Euro(num1);
+        Euro b = new Euro(num2);
+        a.sottrai(b);
+        assertEquals((long)((num1-num2)*100),a.getValore());
+    }
+
+    @Test
+    public void testUgualea(){
+        double num1 = 12345.67;
+        double num2 = 12345.67;
+        Euro a = new Euro(num1);
+        Euro b = new Euro(num2);
+        assertEquals(a.getValore(),b.getValore());
+        assertTrue(a.ugualeA(b));
+    }
+
+    @Test
+    public void testUgualea2(){
+        double num1 = 12345.22;
+        double num2 = 12345.67;
+        Euro a = new Euro(num1);
+        Euro b = new Euro(num2);
+        assertNotEquals(a.getValore(),b.getValore());
+        assertFalse(a.ugualeA(b));
+    }
+
+    @Test
+    public void testMinoreDi_1(){
+        double num1 = 12345.67;
+        double num2 = 12.34;
+        Euro a = new Euro(num1);
+        Euro b = new Euro(num2);
+        assertTrue(a.getValore()>=b.getValore());
+        assertTrue(b.minoreDi(a));
+    }
+
+    @Test
+    public void testMinoreDi_2(){
+        double num1 = 12345.67;
+        double num2 = 12.34;
+        Euro a = new Euro(num1);
+        Euro b = new Euro(num2);
+        assertFalse(a.minoreDi(b));
+    }
+
+    @Test
+    public void stampa(){
+        String s="12345.67 euro";
+        Euro euro=new Euro(12345.67);
+        assertEquals(s,euro.stampa());
+    }
+
+}
