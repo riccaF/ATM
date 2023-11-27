c44ee30f4ab2732de80953c773688861af1e702d
FrancyGee <francesco1.ghio@gmail.com>
2023-11-27 12:23:48 +0100

    Cambiati valori double in Euro con opportune modifiche per il codice

--- src/GUI/Withdrawal.java
+++ src/GUI/Withdrawal.java
@@ -2,6 +2,7 @@ package GUI;
 // Withdrawal.java
 // Represents a withdrawal ATM transaction
 
+import Business_logic.Euro;
 import Business_logic.Transaction;
 import Database.BankDatabase;
 
@@ -31,7 +32,7 @@ public class Withdrawal extends Transaction
    public void execute()
    {
       boolean cashDispensed = false; // cash was not dispensed yet
-      double availableBalance; // amount available for withdrawal
+      Euro availableBalance; // amount available for withdrawal
 
       // get references to bank database and screen
       BankDatabase bankDatabase = getBankDatabase(); 
@@ -51,13 +52,13 @@ public class Withdrawal extends Transaction
                bankDatabase.getAvailableBalance( getAccountNumber() );
       
             // check whether the user has enough money in the account 
-            if ( amount <= availableBalance )
+            if ( amount <= availableBalance.getValore() )
             {   
                // check whether the cash dispenser has enough money
                if ( cashDispenser.isSufficientCashAvailable( amount ) )
                {
                   // update the account involved to reflect withdrawal
-                  bankDatabase.debit( getAccountNumber(), amount );
+                  bankDatabase.debit( getAccountNumber(), new Euro(amount) );
                   
                   cashDispenser.dispenseCash( amount ); // dispense cash
                   cashDispensed = true; // cash was dispensed
