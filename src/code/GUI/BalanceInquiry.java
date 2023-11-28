07c9853080602639ffbbd8beb59ac5ceb61c0a87
matteofazzeri <matteofazzeri@gmail.com>
2023-11-28 16:13:19 +0100

    Sostituzione double -> Euro

--- src/code/GUI/BalanceInquiry.java
+++ src/code/GUI/BalanceInquiry.java
@@ -2,6 +2,7 @@ package code.GUI;
 // BalanceInquiry.java
 // Represents a balance inquiry ATM transaction
 
+import code.Business_logic.Euro;
 import code.Business_logic.Transaction;
 import code.Database.BankDatabase;
 
@@ -22,11 +23,11 @@ public class BalanceInquiry extends Transaction
       Screen screen = getScreen();
 
       // get the available balance for the account involved
-      double availableBalance = 
+      Euro availableBalance = 
          bankDatabase.getAvailableBalance( getAccountNumber() );
 
       // get the total balance for the account involved
-      double totalBalance = 
+      Euro totalBalance = 
          bankDatabase.getTotalBalance( getAccountNumber() );
       
       // display the balance information on the screen
