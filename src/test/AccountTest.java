dc40576a2430670feeb2326d7ba72c9f10e7e410
marti-malcuit <S5260784@studenti.unige.it>
2023-11-28 17:05:00 +0100

    aggiunta librerie

---
+++ src/test/AccountTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class AccountTest {
+    @Test
+    void testCredit() {
+
+    }
+
+    @Test
+    void testDebit() {
+
+    }
+
+    @Test
+    void testGetAccountNumber() {
+
+    }
+
+    @Test
+    void testGetAvailableBalance() {
+
+    }
+
+    @Test
+    void testGetTotalBalance() {
+
+    }
+
+    @Test
+    void testValidatePIN() {
+
+    }
+}
