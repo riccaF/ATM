0d73a0724904ef273167c0f1747dfe48cc63875c
Lorenzo Mecca <5317293@studenti.unige.it>
2023-11-28 16:29:49 +0100

    Aggiunta metodi di test

--- src/test/TestBankDatabase.java
+++ src/test/TestBankDatabase.java
@@ -1,5 +1,46 @@
 package test;
 
+import code.Business_logic.Euro;
+import code.Database.BankDatabase;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestBankDatabase {
-	
+
+    private BankDatabase bankDatabase;
+
+    @Before
+    public void setUp() {
+        bankDatabase = new BankDatabase();
+    }
+
+    @Test
+    public void testAuthenticateUser() {
+        Assert.assertTrue(bankDatabase.authenticateUser(12345, 54321));
+        Assert.assertFalse(bankDatabase.authenticateUser(12345, 12345));
+    }
+
+    @Test
+    public void testGetAvailableBalance() {
+        Assert.assertEquals(100000, bankDatabase.getAvailableBalance(12345).getValore());
+    }
+
+    @Test
+    public void testGetTotalBalance() {
+        Assert.assertEquals(120000, bankDatabase.getTotalBalance(12345).getValore());
+    }
+
+    @Test
+    public void testCredit() {
+        bankDatabase.credit(12345, new Euro(500, 0));
+        Assert.assertEquals(170000, bankDatabase.getTotalBalance(12345).getValore());
+    }
+
+    @Test
+    public void testDebit() {
+        bankDatabase.debit(12345, new Euro(500, 0));
+        Assert.assertEquals(50000, bankDatabase.getAvailableBalance(12345).getValore());
+    }
 }
--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -1,5 +1,38 @@
 package test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import code.Business_logic.Euro;
+
 public class TestEuro {
 
+    @Test
+    public void testSomma() {
+        Euro euro1 = new Euro(5, 50);
+        Euro euro2 = new Euro(4, 50);
+        euro1.somma(euro2);
+        Assert.assertEquals(1000, euro1.getValore());
+    }
+
+    @Test
+    public void testSottrai() {
+        Euro euro1 = new Euro(5, 50);
+        Euro euro2 = new Euro(4, 50);
+        euro1.sottrai(euro2);
+        Assert.assertEquals(100, euro1.getValore());
+    }
+
+    @Test
+    public void testUgualeA() {
+        Euro euro1 = new Euro(5, 50);
+        Euro euro2 = new Euro(5, 50);
+        Assert.assertTrue(euro1.ugualeA(euro2));
+    }
+
+    @Test
+    public void testMinoreDi() {
+        Euro euro1 = new Euro(4, 50);
+        Euro euro2 = new Euro(5, 50);
+        Assert.assertTrue(euro1.minoreDi(euro2));
+    }
 }
