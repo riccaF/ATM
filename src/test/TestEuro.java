caa3fb89ccef3f2b9b1092b3129f5ec79e37900f
MattiaCacciatore <cacciatore1995@hotmail.it>
2023-12-04 22:45:57 +0100

    Commit (4.1) Aggiunti tests - fallimento metodo MinoreDi

--- src/test/TestEuro.java
+++ src/test/TestEuro.java
@@ -19,6 +19,8 @@ public class TestEuro {
     @Test
     public void testGetValore() {
         assertEquals(5075, euro.getValore());
+        assertNotEquals(5076, euro.getValore());
+        assertNotEquals(5074, euro.getValore());
     }
 
     @Test
@@ -27,6 +29,8 @@ public class TestEuro {
         Euro risultato = euro.somma(altroEuro);
 
         assertEquals(76.25, risultato.getValore() / 100.0, 0.01);
+        assertNotEquals(77.26, risultato.getValore() / 100.0, 0.01);
+        assertNotEquals(75.25, risultato.getValore() / 100.0, 0.01);
     }
 
     @Test
@@ -35,34 +39,50 @@ public class TestEuro {
         Euro risultato = euro.sottrai(altroEuro);
 
         assertEquals(25.25, risultato.getValore() / 100.0, 0.01);
+        assertNotEquals(25.26, risultato.getValore() / 100.0, 0.01);
+        assertNotEquals(24.25, risultato.getValore() / 100.0, 0.01);
     }
 
     @Test
     public void testUgualeA() {
         Euro stessoEuro = new Euro(50, 75);
+        Euro euroMaggiore = new Euro(50, 76);
+        Euro euroMinore = new Euro(50, 74);
 
         assertTrue(euro.ugualeA(stessoEuro));
+        assertFalse(euro.ugualeA(euroMaggiore));
+        assertFalse(euro.ugualeA(euroMinore));
     }
 
     @Test
     public void testMinoreDi() {
-        Euro altroEuro = new Euro(75, 0);
+        Euro euroMaggiore = new Euro(75, 0);
+        Euro stessoEuro = new Euro(50, 75);
+        Euro euroMinore = new Euro(50, 74);
 
-        assertTrue(euro.minoreDi(altroEuro));
+        assertTrue(euro.minoreDi(euroMaggiore));
+        assertFalse(euro.minoreDi(stessoEuro)); // Ci si aspetta fallisca.
+        assertFalse(euro.minoreDi(euroMinore));
     }
 
     @Test
     public void testCostruttoreDouble() {
         double valoreDouble = 20.75;
         long valoreCent = 2075;
+        long valoreCentErrato = 2076;
+        long valoreCentErrato2 = 2074;
 
         Euro euroCostruito = new Euro(valoreDouble);
 
         assertEquals(valoreCent, euroCostruito.getValore());
+        assertNotEquals(valoreCentErrato, euroCostruito.getValore());
+        assertNotEquals(valoreCentErrato2, euroCostruito.getValore());
     }
 
     @Test
     public void testStampa() {
         assertEquals("50.75 euro", euro.stampa());
+        assertNotEquals("50.76 euro", euro.stampa());
+        assertNotEquals("50.74 euro", euro.stampa());
     }
 }
